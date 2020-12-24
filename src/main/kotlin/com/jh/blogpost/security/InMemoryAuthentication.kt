2c3cbe1d0b30cb08511adf97939574214478cd7b
Ubuntu <jayamalah@gmail.com>
2020-12-24 19:35:53 +0000

    code cleanup

--- src/main/kotlin/com/jh/blogpost/security/InMemoryAuthentication.kt
+++ src/main/kotlin/com/jh/blogpost/security/InMemoryAuthentication.kt
@@ -1,19 +1,11 @@
 package com.jh.blogpost.security
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
-import org.springframework.security.config.annotation.web.builders.WebSecurity
-
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import java.lang.Exception
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
-
-import org.springframework.security.crypto.password.PasswordEncoder
-
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 
--- src/test/kotlin/com/jh/blogpost/security/InMemoryBasicAuthenticationTests.kt
+++ src/test/kotlin/com/jh/blogpost/security/InMemoryBasicAuthenticationTests.kt
@@ -1,31 +1,23 @@
 package com.jh.blogpost.security
 
-import junit.framework.Assert.assertEquals
-import junit.framework.Assert.assertTrue
-import org.junit.Before
-import org.junit.Test
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
-import org.springframework.http.HttpStatus
-
-import org.springframework.http.ResponseEntity
-
-import org.springframework.boot.test.web.client.TestRestTemplate
-
 import java.io.IOException
-
 import java.net.MalformedURLException
-
-import org.springframework.boot.web.server.LocalServerPort
-
-import org.springframework.boot.test.context.SpringBootTest
-
-import org.springframework.test.context.junit4.SpringRunner
-
-import org.junit.runner.RunWith
-import org.springframework.beans.factory.annotation.Autowired
 import java.lang.Exception
 import java.net.URL
+
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.beans.factory.annotation.Autowired
+
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
