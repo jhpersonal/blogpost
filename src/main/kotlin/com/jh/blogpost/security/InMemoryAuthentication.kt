44b0f2f9cfedb4757746d5f65aaf650439f80d08
Ubuntu <jayamalah@gmail.com>
2021-01-09 03:57:40 +0000

    Moved inmemory authentication with security expressions to a different file and have the basic in-memory authentication in the original file

--- src/main/kotlin/com/jh/blogpost/security/InMemoryAuthentication.kt
+++ src/main/kotlin/com/jh/blogpost/security/InMemoryAuthentication.kt
@@ -1,37 +1,41 @@
 package com.jh.blogpost.security
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import java.lang.Exception
-import org.springframework.beans.factory.annotation.Autowired
-
-
-
 
 @Configuration
 @EnableWebSecurity
 class InMemoryAuthentication: WebSecurityConfigurerAdapter() {
 
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        val encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
+
+        // This code sets up a user store in memory.
+        auth.inMemoryAuthentication()
+            .withUser("editor")
+            .password(encoder.encode("password"))
+            .roles("USER")
+            .and()
+
+            .withUser("author")
+            .password(encoder.encode("password"))
+            .roles("ADMIN")
+    }
+
     override fun configure(http: HttpSecurity) {
         //This tells Spring Security to authorize all requests
-        http.httpBasic()
+        http
+            .httpBasic()
             .and()
-            .authorizeRequests()
-            .antMatchers("/v3/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
-            .antMatchers(HttpMethod.GET, "/posts/{id}/author").hasRole("EDITOR")
-            .antMatchers(HttpMethod.DELETE, "/posts/{id}").hasRole("EDITOR")
-            .antMatchers(HttpMethod.GET,"/posts").hasAnyRole("EDITOR", "AUTHOR")               // (HttpMethod.GET, "/", "/publication").permitAll()
-            .antMatchers(HttpMethod.POST, "/posts").hasAnyRole("EDITOR","AUTHOR")
-                            // role-based authorization
-            .anyRequest().authenticated()    //.permitAll()
-//            .and().logout().permitAll().logoutRequestMatcher( AntPathRequestMatcher("/logout", HttpMethod.GET.toString())).invalidateHttpSession(true)
-            .and()
+                .authorizeRequests()
+                .antMatchers("/v3/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
             .csrf().disable()   // disable Spring Security built-in cross-site scripting protection.
             .formLogin().disable()  // disable default login form
 
@@ -39,23 +43,6 @@ class InMemoryAuthentication: WebSecurityConfigurerAdapter() {
         // BCrypt is a strong hashing algorithm recommended by Spring Security.
     }
 
-    @Throws(Exception::class)
-    override fun configure(auth: AuthenticationManagerBuilder) {
-        val encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder()
-
-        // This code sets up a user store in memory.
-        auth.inMemoryAuthentication()
-            .withUser("editor").password(encoder.encode("pass")).roles("EDITOR")
-            .and()
-            .withUser("author").password(encoder.encode("pass")).roles("AUTHOR")
-    }
 }
 
 
-
-
-
-//    @Bean
-//    fun encoder(): PasswordEncoder {
-//        return BCryptPasswordEncoder()
-//    }
--- src/test/kotlin/com/jh/blogpost/security/InMemoryBasicAuthenticationTests.kt
+++ src/test/kotlin/com/jh/blogpost/security/InMemoryBasicAuthenticationTests.kt
@@ -1,8 +1,5 @@
 package com.jh.blogpost.security
 
-import java.io.IOException
-import java.net.MalformedURLException
-import java.lang.Exception
 import java.net.URL
 
 import junit.framework.Assert.assertEquals
@@ -30,14 +27,12 @@ class InMemoryBasicAuthenticationTests {
 
 
     @Before
-    @Throws(MalformedURLException::class)
     fun setUp() {
-        restTemplate = TestRestTemplate("user", "password")
+        restTemplate = TestRestTemplate("editor", "password")
         base = URL("http://localhost:$port")
     }
 
     @Test
-    @Throws(IllegalStateException::class, IOException::class)
     fun whenLoggedUser_ThenSuccess() {
         val response: ResponseEntity<String> = restTemplate.getForEntity(base.toString(), String::class.java)
         assertEquals(HttpStatus.OK, response.statusCode)
@@ -45,18 +40,28 @@ class InMemoryBasicAuthenticationTests {
     }
 
     @Test
-    @Throws(Exception::class)
-    fun withWrongCredentials_thenUnauthorizedPage() {
-        restTemplate = TestRestTemplate("user", "wrongpassword")
-        val response: ResponseEntity<String> = restTemplate.getForEntity(base.toString(), String::class.java)
-        assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+    fun whenLoggedUser_Approach2_ThenSuccess() {
+        restTemplate = TestRestTemplate()
+        val response: ResponseEntity<String> = restTemplate
+            .withBasicAuth("editor", "password")
+            .getForEntity(base.toString(), String::class.java)
+        assertEquals(HttpStatus.OK, response.statusCode)
+        println(response.body)
     }
 
     @Test
-    @Throws(Exception::class)
+    fun withWrongCredentials_thenUnauthorizedPage() {
+        restTemplate = TestRestTemplate("editor", "wrongpassword")
+        val response: ResponseEntity<String> = restTemplate.getForEntity(base.toString(), String::class.java)
+        assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+        println(response.statusCode)
+    }
+
+    @Test
     fun withoutAuthentication_thenUnauthorizedPage() {
         restTemplate = TestRestTemplate()
         val response: ResponseEntity<String> = restTemplate.getForEntity(base.toString(), String::class.java)
         assertEquals(HttpStatus.UNAUTHORIZED, response.statusCode)
+        println(response.statusCode)
     }
 }
