56a5da7f51a334360a02829aa7c95b21e3a9ee06
Ubuntu <jayamalah@gmail.com>
2021-02-04 01:15:34 +0000

    singleton pattern specific example with comments

--- src/main/kotlin/com/jh/blogpost/designpatterns/singleton/singleton.kt
+++ src/main/kotlin/com/jh/blogpost/designpatterns/singleton/singleton.kt
@@ -1,28 +1,34 @@
 package com.jh.blogpost.designpatterns.singleton
 
-
-
-object Singleton {
+// Singleton Class in Kotlin is also called as the Singleton Object in Kotlin.
+// Singleton class is a class that is defined in such a way that only one instance of the class can be created and used everywhere.
+// When we use an object instead of a class, Kotlin actually uses the Singelton and allocates the single memory.
+// The object class can have functions, properties, and the init method.
+// The constructor method is not allowed in an object so we can use the init method if some initialization is required and the object can be defined inside a class.
+// We call the method and member variables present in the singleton class using the class name.
+// Examples: to maintain a single instance of resources like a database or object store or file system.
+object DatabaseInstance {
     init {
-        println("Singleton initialized")
+        println("Singleton initialized the default database 'defaultdb'")
     }
 
-    var message = "Kotlin rock"
+    var database = "defaultdb"
 
-    fun showMessage() {
-        println(message)
+    fun setConnection() {
+        println(database)
     }
 }
 
-class InvokeSingleton {
+// Invoke Singleton object
+class Singleton {
     init {
-        Singleton.showMessage()
+        DatabaseInstance.setConnection()
     }
 }
 
 fun main() {
-    Singleton.showMessage()
-    Singleton.message = "Kotlin is cool"
+    DatabaseInstance.setConnection()
+    DatabaseInstance.database = "blogpostdb"
 
-    InvokeSingleton()
+    Singleton()
 }
